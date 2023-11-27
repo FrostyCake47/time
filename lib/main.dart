00b6a4606e5bae2ba7f9994f131b83d484ae09d9
Akash <AwesomeAkash47@gmail.com>
2023-11-27 23:33:12 +0530

    Transformed cards

--- lib/main.dart
+++ lib/main.dart
@@ -45,114 +45,120 @@ class _HomeState extends State<Home> {
           child: Container(
             width: MediaQuery.of(context).size.width,
             child: Column(
-              children: [Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(45.0),//or 15.0
-                    child: Container(
-                      height: 70.0,
-                      width: 70.0,
-                      child: Image(image: AssetImage("assets/PaperPlanes1.jpg")),
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-              
-                  Text(
-                    "NAME",
-                    style: TextStyle(
-                      color: Colors.grey[200]
-                    )
-                  ),
-                  //SizedBox(height: 5,),
-                  Text(
-                    "Akash",
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontFamily: "Pacifico",
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-              
-                  SizedBox(height: 10,),
-                  Text(
-                    "Current Level",
-                    style: TextStyle(
-                      color: Colors.grey[200]
-                    )
-                  ),
-                  //SizedBox(height: 10,),
-                  Text(
-                    "$level",
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontFamily: "Pacifico",
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  
-                  ],
-                ),
-                SizedBox(height: 40,),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.email,
-                            color: Colors.grey[200],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children:[
+                    Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(45.0),//or 15.0
+                          child: Container(
+                            height: 70.0,
+                            width: 70.0,
+                            child: Image(image: AssetImage("assets/PaperPlanes1.jpg")),
                           ),
-                          SizedBox(width: 10,),
-                          Text(
-                            "Awesomeakash47@gmail.com",
-                            style: TextStyle(
-                              color: Colors.grey[200]
-                            )
+                        ),
+                        SizedBox(height: 10,),
+                    
+                        Text(
+                          "NAME",
+                          style: TextStyle(
+                            color: Colors.grey[200]
+                          )
+                        ),
+                        //SizedBox(height: 5,),
+                        Text(
+                          "Akash",
+                          style: TextStyle(
+                            color: Colors.amber,
+                            fontFamily: "Pacifico",
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
                           ),
+                        ),
+                    
+                        SizedBox(height: 10,),
+                        Text(
+                          "Current Level",
+                          style: TextStyle(
+                            color: Colors.grey[200]
+                          )
+                        ),
+                        //SizedBox(height: 10,),
+                        Text(
+                          "$level",
+                          style: TextStyle(
+                            color: Colors.amber,
+                            fontFamily: "Pacifico",
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        
                         ],
                       ),
+                      SizedBox(height: 40,),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.email,
+                                  color: Colors.grey[200],
+                                ),
+                                SizedBox(width: 10,),
+                                Text(
+                                  "Awesomeakash47@gmail.com",
+                                  style: TextStyle(
+                                    color: Colors.grey[200]
+                                  )
+                                ),
+                              ],
+                            ),
+                        
+                            SizedBox(height: 10,),
+                            
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.phone,
+                                  color: Colors.grey[200],
+                                ),
+                                SizedBox(width: 10,),
+                                Text(
+                                  "9746796942",
+                                  style: TextStyle(
+                                    color: Colors.grey[200]
+                                  )
+                                ),
+                                
+                              ],
+                            ),
+                            SizedBox(height: 20,),
                   
-                      SizedBox(height: 10,),
-                      
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.phone,
-                            color: Colors.grey[200],
-                          ),
-                          SizedBox(width: 10,),
-                          Text(
-                            "9746796942",
-                            style: TextStyle(
-                              color: Colors.grey[200]
-                            )
-                          ),
-                          
-                        ],
-                      ),
-                      SizedBox(height: 20,),
-
-                      Text(
-                        "Favourite",
-                        style: TextStyle(
-                          color: Colors.grey[200]
-                        )
-                      ),
-
-                      
-                    ],
-                  ),
+                            Text(
+                              "Favourite",
+                              style: TextStyle(
+                                color: Colors.grey[200]
+                              )
+                            ),
                   
+                            
+                          ],
+                        ),
+                        
+                      ),
+                      
+                  ]
                 ),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: songList.map((songs) => songs).toList()
                 )
-
-              ]
+              ],
             ),
           ),
         ),
